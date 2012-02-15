12fea65e70c4fcf6abc6f0b7905f41bf9b2d1791
AutomatedTester <david.burns@theautomatedtester.co.uk>
2012-02-15 21:53:46 +0000

    initial commit with project structure

---
+++ AutomatedTester.BrowserMob/Client.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace AutomatedTester.BrowserMob
+{
+    public class Client
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        public Client(String url)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void NewHar()
+        {
+            NewHar("");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="reference"></param>
+        public void NewHar(String reference)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="reference"></param>
+        public void NewPage(String reference)
+        {
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Dictionary<String, T> Har
+        {
+            get
+            {
+                
+            }
+        }   
+
+        public void Limits(Dictionary<String, int> options)
+        {
+            
+        }
+
+        public void WhiteList(String regexp, int statusCode)
+        {
+            
+        }
+        public void Blacklist(String regexp, int statusCode)
+        {
+            
+        }
+       
+
+        /// <summary>
+        /// shuts down the proxy and closes the port
+        /// </summary>
+        public void Close()
+        {
+            
+        }
+
+    }
+}
---
+++ AutomatedTester.BrowserMob/Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutomatedTester.BrowserMob
+{
+    public class Server
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        public Server(String path)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="port"></param>
+        public Server(String path, int port)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Start()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Stop()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Client CreateClient(){
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private bool IsListening
+        {
+            get { }
+
+        }
+    }
+}
